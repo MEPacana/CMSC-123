c397728735b7cb64c41fb886d76a4c40b7df943a
Michael Ervin Pacana <michaelbinpacs@gmail.com>
2016-10-03 11:48:39 +0800

    Update linked list.cpp

--- linked list.cpp
+++ linked list.cpp
@@ -1,6 +1,43 @@
-   for(int i = 0; n.insert(i, temp->elem); i++, temp = temp->next);
-    return n;
-}*/
+#include <iostream>
+using namespace std;
+template <class T>
+class node{
+public:
+    T elem;
+    node* next;
+    node();
+    node(T n){elem = n; next = NULL;}
+};
+
+template <class T>
+class list{
+public:
+    int size;
+    node<T> *head,*tail;
+public:
+    list();
+    list(const list&);
+    list& operator=(const list&);
+    bool insert(int, T);
+    T get(int);
+};
+
+template <class T>
+void display(list<T>);
+
+template <class T>
+list<T>::list(){
+    size = 0;
+    head = tail = NULL;
+}
+
+template <class T>
+list<T>::list(const list<T>& l){
+    size = 0;
+    head = tail = NULL;
+    node<T> *temp = l.head;
+    for(int i = 0; insert(i,temp->elem); i++, temp = temp->next);
+}
 
 template <class T>
 bool list<T>::insert(int i, T n){
@@ -14,14 +51,16 @@ bool list<T>::insert(int i, T n){
     else if(i==size){
         tail->next = x;
         tail = x;
-    } 
+    }
     else if(i == 0){
         x->next = head;
         head = x;
+        cout<<"tomato"<<endl;
     }
     else {
         node<T> *temp = head;
-            for(int j = 0; j < i-1; j++){
+        cout<<"tomato2"<<endl;
+        for(int j = 0; j < i-1; j++){
                 temp = temp->next;
             }
             x->next = temp->next;
@@ -44,13 +83,15 @@ int main(){
     list<int> List;
     int tst, pos, item;
     cin>>tst;
-    while(tst>0){         
+    while(tst>0){
         cin>>pos;
-       
+        cout<<"potato";
         cin>>item;
-          
+        cout<<"potato1";
         List.insert(pos, item);
+        cout<<"potato2";
         display(List);
+        cout<<"potato";
         tst--;
     }
     return 0;
@@ -63,3 +104,4 @@ void display(list<T> l){
     }
     cout<<endl;
 }
+
